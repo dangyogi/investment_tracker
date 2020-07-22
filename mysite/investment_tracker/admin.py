faa3993a64fd7d82cf0823c6fd96b2b27ad759d0
Bruce Frederiksen <dangyogi@gmail.com>
2020-07-22 13:10:10 -0400

    Set up list options on admin screens.

--- mysite/investment_tracker/admin.py
+++ mysite/investment_tracker/admin.py
@@ -2,9 +2,44 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Account, Category, Plan
+from .models import (
+    User, Account, Category, CategoryLink, Plan, CategoryFund, Fund
+)
 
 
-admin.site.register(Account)
-admin.site.register(Category)
-admin.site.register(Plan)
+admin.site.register(User)
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner')
+    ordering = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+
+
+@admin.register(CategoryLink)
+class CategoryLinkAdmin(admin.ModelAdmin):
+    list_display = ('parent', 'child', 'owner', 'account', 'order')
+    ordering = ('parent__name', 'order')
+
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('category', 'owner', 'account', '__str__')
+    ordering = ('category__name', 'owner__name', 'account__name')
+
+
+@admin.register(CategoryFund)
+class CategoryFundAdmin(admin.ModelAdmin):
+    list_display = ('category', 'owner', 'account', 'fund')
+    ordering = ('category__name', 'owner__name', 'account__name')
+
+
+@admin.register(Fund)
+class FundAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ticker')
+    ordering = ('name',)
